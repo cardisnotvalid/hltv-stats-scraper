356bfeb46cc4d0b6afba1f1710025dd565c6e663
Byte <deadcardinal293@gmail.com>
2023-08-17 05:02:00 +0700

    Добавлен функция сохранение данных

--- stats_scraper/utils.py
+++ stats_scraper/utils.py
@@ -1,7 +1,22 @@
+import os
 import yaml
-from stats_scraper.paths import CONFIG_PATH
+import json
+
+from stats_scraper.logger import logger
+from stats_scraper.paths import CONFIG_PATH, OUT_DIR
 
 
 def load_config() -> dict:
     with open(CONFIG_PATH) as file:
         return yaml.load(file, Loader=yaml.FullLoader)
+
+
+def save_data(match_name: str, filename: str, json_data: dict) -> None:
+    filename = f"{filename}.json"
+    filepath = OUT_DIR / match_name / filename
+    
+    os.makedirs(OUT_DIR / match_name, exist_ok=True)
+    
+    with open(filepath, "w", encoding="utf-8") as file:
+        json.dump(json_data, file, ensure_ascii=False, indent=4)
+    logger.info(f"Файл сохранен {match_name}/{filename}")
